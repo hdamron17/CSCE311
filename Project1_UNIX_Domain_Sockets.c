f197df745e7e23608b86cde2f895d6c22838eb74
Unknown <mcdonag@email.sc.edu>
2018-10-14 20:41:21 -0400

    I'm still working on the file IO and testing. I'm storing the lines with the word in them as strings in an array. Then, I'll sort the array and print it.

--- Project1_UNIX_Domain_Sockets.c
+++ Project1_UNIX_Domain_Sockets.c
@@ -10,11 +10,27 @@
 #include <sys/socket.h> //Beej
 #include <sys/wait.h> //Beej
 
+typedef char * string;
+
 int main(int argc, char **argv)
 {
 	int socket[2];  //  Sockets
 	char buffer;  //  Buffer for interprocess data
 	FILE *fp;  //  Pointer for reading file
+	fp = fopen(argv[1], "r"); // takes file from cmd line
+	int lines = 0; //  Number of lines in file
+
+	//  Find number of lines in file.
+	while(!feof(fp))
+	{
+		int current = fgetc(fp);
+		if(current == "\n")
+		{
+			lines = lines + 1;
+		}
+	}
+
+	string true_lines[lines];  //  Array of strings with the word
 
 	if (socketpair(AF_UNIX, SOCK_STREAM, 0, socket) == -1) {  //  Beej
 		perror("socketpair");
@@ -26,30 +42,20 @@ int main(int argc, char **argv)
 		//  CHILD PROCESS
 		read(socket[1], &buffer, 1);
       
-    const char word[80];  // word of interest in file
-    printf("Enter the word of interest: ");
-    scanf("%s", word);
-    while (fp && fgets(tmp, sizeof(tmp), fp))  // fgets() -- get string from stream
-    {
-      if (strstr(tmp, word))  // strstr() -- locates substring
-        if (strcasestr(tmp, word) || strcasecmp(tmp, word) == 0)  // strcasestr() -- locates substring ignoring case
-          printf("%s", tmp);
-    }
-    fclose(fp);  // fclose() -- close the file stream
 
 		write(socket[1], &buffer, 1);
-		printf("child: sent '%c'\n", buffer);
 
 	}
 	else
 	{
 		//  PARENT PROCESS
-    *fp = fopen(argv[1], "r"); // takes file from cmd line
-    const char tmp[256] = { 0x0 };  // used to scan entire file
+    fp = fopen(argv[1], "r"); // takes file from cmd line
 
-		write(socket[0], "b", 1);  //  TODO: Change to write file info
+		write(socket[0], "bc", 1);  //  Write current line to buffer
 
-		read(socket[0], &buffer, 1);
+		read(socket[0], &buffer, 1);  //  Read current line from buffer
+
+		printf("Test: %s", buffer);
 
 		wait(NULL);  //  Wait for child process completion
 	}
